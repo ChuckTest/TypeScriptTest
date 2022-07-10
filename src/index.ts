b14afcb365b685fc57b276a31f0b41a57ca70a89
Chuck LU <chucklu2015@outlook.com>
2022-07-10 14:54:08 +0800

    object with function property

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@ let employee: {
     readonly id: number,
     name: string,
     retire: (date: Date) => void
-} = { id: 1, name: 'Chuck' };
+} = {
+    id: 1,
+    name: 'Chuck',
+    retire: (date: Date) => {
+        console.log(date);
+    }
+};
